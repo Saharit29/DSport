8029f30601bc36edc35c79e8d06f297089b983b8
lacakp <lacakp.contact@gmail.com>
2021-11-21 18:02:11 +0700

    Update

---
+++ sport/sport/FormAppReport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sport
+{
+    public partial class FormAppReport : Form
+    {
+        public FormAppReport()
+        {
+            InitializeComponent();
+        }
+
+        cryAppReport rptCryApp = new cryAppReport();
+        private void FormAppReport_Load(object sender, EventArgs e)
+        {
+            FormFilterApp Frm = new FormFilterApp();
+
+            cryViewer.ReportSource = rptCryApp;
+            
+        }
+    }
+}
